db2fd486f14cfe6c40454892b4bcc0aed9d96bd3
ozzy <businessozzy27@gmail.com>
2025-09-27 10:32:25 -0400

    multiplayer

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -2,35 +2,44 @@ extends CharacterBody2D
 @onready var sprite = $Sprite
 @onready var levelCam=$"../Camera"
 
-# Character Stats
 @export var moveSpeed = 250
 @export var acceleration = 2000
-@export var friction = 1000 
-@export var playerNum=0
+@export var friction = 1000
+@export var deviceID = 0
+
+@export var movement_deadzone: float = 0.15
 
 func _ready():
 	pass
 
 func _physics_process(delta):
-	var input_vector = Vector2.ZERO
-	input_vector.x = Input.get_axis("dirLeft", "dirRight")
-	input_vector.y = Input.get_axis("dirUp", "dirDown")
-	input_vector = input_vector.normalized()
+	var suffix = str(deviceID)
+	var action_left  = "dirLeft" + suffix
+	var action_right = "dirRight" + suffix
+	var action_up    = "dirUp" + suffix
+	var action_down  = "dirDown" + suffix
 	
-	var target_velocity = input_vector * moveSpeed
+	var input_vector = Input.get_vector(
+		action_left, 
+		action_right, 
+		action_up, 
+		action_down, 
+		deviceID
+	)
 	
-	if input_vector.length() > 0:
-		velocity = velocity.move_toward(target_velocity, acceleration * delta)
+	if input_vector.length() > movement_deadzone:
+		velocity = velocity.move_toward(input_vector * moveSpeed, acceleration * delta)
+		
+		sprite.play("Running")
+		if velocity.x < 0:
+			sprite.flip_h=true
+		elif velocity.x > 0:
+			sprite.flip_h=false
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
+		
+		sprite.play("Standing")
+		
 	move_and_slide()
 	
-	# ANIMATIONS
-	if velocity.length() < 10: 
-		sprite.play("Standing")
-	else:
-		sprite.play("Running")
-		if velocity.x<0:
-			sprite.flip_h=true
-		if velocity.x>0:  #if 0, it doesnt change
-			sprite.flip_h=false
+	print(input_vector.x," & ",input_vector.y)
