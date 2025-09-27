d68f38afeb4ec3eb73b6792bbe6a2eddc829a5c2
ozzy <businessozzy27@gmail.com>
2025-09-27 13:30:57 -0400

    bullets

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -5,18 +5,21 @@ extends CharacterBody2D
 @export var acceleration = 2000
 @export var friction = 1000
 @export var deviceID = 0
+@export var bulletSpeed = 400
 
 @export var movement_deadzone: float = 0.15
+var bullet = load("res://Scenes/bullet.tscn")
+var last_input_vector = Vector2.ZERO
 
 func _ready():
 	pass
 
 func _physics_process(delta):
 	var suffix = str(deviceID)
-	var action_left  = "dirLeft" + suffix
+	var action_left = "dirLeft" + suffix
 	var action_right = "dirRight" + suffix
-	var action_up    = "dirUp" + suffix
-	var action_down  = "dirDown" + suffix
+	var action_up = "dirUp" + suffix
+	var action_down = "dirDown" + suffix
 	
 	var input_vector = Input.get_vector(
 		action_left, 
@@ -25,6 +28,8 @@ func _physics_process(delta):
 		action_down, 
 		deviceID
 	)
+	
+	last_input_vector = input_vector
 	
 	if input_vector.length() > movement_deadzone:
 		velocity = velocity.move_toward(input_vector * moveSpeed, acceleration * delta)
@@ -39,4 +44,18 @@ func _physics_process(delta):
 		
 		sprite.play("Standing")
 		
+	
+	if Input.is_action_just_pressed("Action"+suffix):
+		summonBullet()
+
 	move_and_slide()
+
+func summonBullet():
+	var newBullet = bullet.instantiate()
+	newBullet.position = position
+	newBullet.direction = last_input_vector
+	if last_input_vector.x<0.1 and last_input_vector.y<0.1:
+		newBullet.direction.x = -1 if sprite.flip_h else 1
+		newBullet.direction.y = 0
+	newBullet.modifier = bulletSpeed
+	get_parent().add_child(newBullet)
