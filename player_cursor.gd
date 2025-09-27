c1ca276116da6b29991367be2f3c0e8c78288088
Ultrablob <neelgillshah@gmail.com>
2025-09-27 15:55:04 -0400

    working on upgrades

--- player_cursor.gd
+++ player_cursor.gd
@@ -4,7 +4,6 @@ extends Sprite2D
 @export var speed = 10
 
 func _process(delta):
-	
 	if Input.is_action_just_pressed("Action0") and $Area2D.get_overlapping_areas():
 		$Area2D.get_overlapping_areas()[0].grabber = self
 		$Area2D.get_overlapping_areas()[0].valid = false
