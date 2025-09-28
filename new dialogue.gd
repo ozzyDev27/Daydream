7a0a350ac7f26363d4e4c56ee4128dbb99ca2d50
Furiouspotato <realfuriouspotato@gmail.com>
2025-09-28 17:30:55 -0400

    lastminute

--- new dialogue.gd
+++ new dialogue.gd
@@ -1,13 +1,21 @@
 extends Node2D
 
-var dialogues = [".", "Well, our ship is long gone, and we WERE just on our way to clean up earth 2099.", "Lets get cleaning!", "Press Spacebar or \"A\" to slash" ]
-var dialoguesAmount = 0;
+var dialogues = [
+	"Lets not forget our upgrades!",
+	"Drag the blocks into the cells to become stronger!",
+	"Press X or 'L' to move the blocks",
+]
+var dialoguesAmount = 0
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _ready() -> void:
+	# Show the first dialogue immediately
+	$Dialogue1.bbcode_text = dialogues[dialoguesAmount]
+
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("Jump") and len(dialogues) < dialoguesAmount:
-		dialoguesAmount = dialoguesAmount + 1
-		$AnimationPlayer.play("text scroll")
-		$Dialogue1.bbcode_text = dialogues[dialoguesAmount]
-	elif Input.is_action_just_pressed("Jump") and !(len(dialogues) < dialoguesAmount):
-		self.hide()
+	if Input.is_action_just_pressed("Jump"):
+		if dialoguesAmount < len(dialogues) - 1:
+			dialoguesAmount += 1
+			$AnimationPlayer.play("text scroll")
+			$Dialogue1.bbcode_text = dialogues[dialoguesAmount]
+		else:
+			self.hide()
