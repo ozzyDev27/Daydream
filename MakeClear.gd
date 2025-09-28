c9c61bec70c4f17bc7d19ae074d543bda221cd99
Furiouspotato <realfuriouspotato@gmail.com>
2025-09-28 09:57:02 -0400

    Finished Main Menu

---
+++ MakeClear.gd
@@ -0,0 +1,6 @@
+extends TileMapLayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	modulate.a = 0.7
--- Scenes/player_mouse.gd
+++ Scenes/player_mouse.gd
@@ -29,8 +29,9 @@ func _physics_process(delta):
 		action_down, 
 		deviceID
 	)
-	if Input.is_action_just_pressed("DebugAction") and deviceID==0:
+	if Input.is_action_just_pressed("Jump") and deviceID==0:
 		nextLevel()
+		
 	if input_vector.length() > movement_deadzone:
 		velocity = velocity.move_toward(input_vector * moveSpeed, acceleration * delta)
 		
