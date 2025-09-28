1c17c8363608e28cd8416be739071e23f77a675e
ozzy <businessozzy27@gmail.com>
2025-09-28 12:28:10 -0400

    working on mud

---
+++ Scenes/mud.gd
@@ -0,0 +1,65 @@
+extends CharacterBody2D
+
+@export var speed = 40
+@onready var nav_agent = $NavigationAgent2D
+@onready var sprite = $Sprite2D
+var splat = load("res://mud_splat.tscn")
+var dash = 0
+var health = 3
+var nearest_player: Node2D = null
+
+func get_nearest_player():
+	var nearest_distance = 100000
+	for player in get_tree().get_nodes_in_group("players"):
+		var distance = global_position.distance_to(player.global_position)
+		if distance < nearest_distance:
+			nearest_distance = distance
+			nearest_player = player
+
+func _physics_process(delta):
+	if nearest_player:
+		nav_agent.target_position = nearest_player.global_position
+		var target_dir = (nav_agent.get_next_path_position() - global_position).normalized()
+		if dash<1:
+			dash=300
+			velocity = target_dir * speed
+			var newSplat = splat.instantiate()
+			newSplat.position = position
+			get_parent().add_child(newSplat)
+			#position+=velocity
+		elif dash>120:
+			dash-=60*delta
+			velocity*=.95
+		else:
+			velocity=Vector2.ZERO
+			dash-=60*delta
+		if dash<30:
+			sprite.play("charge")
+		elif dash<250:
+			sprite.play("normal")
+		else:
+			sprite.play("go")
+	move_and_slide()
+	get_nearest_player()
+
+# hit by player
+func _on_area_2d_body_entered(body):
+	body.health -= 1
+	body.damage_flash()
+	queue_free() # TODO VFX
+
+func damage_flash():
+	var tween = create_tween()
+	tween.tween_property(self, "modulate", Color(10, 10, 10), 0.03)
+	tween.tween_property(self, "modulate", Color.WHITE, 0.03)
+	#tween.tween_callback(tween.queue_free)
+
+func damage(amount: int):
+	health -= amount
+	damage_flash()
+	if health <= 0:
+		queue_free() # TODO death FX
+
+
+func _on_timer_timeout() -> void:
+	pass # Replace with function body.
