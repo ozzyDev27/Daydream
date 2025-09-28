7487856561ea186c697e61a3c8fe6ca2ab856d8a
ozzy <businessozzy27@gmail.com>
2025-09-28 14:01:51 -0400

    layers

--- player_cursor.gd
+++ player_cursor.gd
@@ -4,10 +4,10 @@ extends Sprite2D
 @export var speed = 10
 
 func _process(delta):
-	if Input.is_action_just_pressed("Action" + str(deviceID)) and $Area2D.get_overlapping_areas():
+	if Input.is_action_just_pressed("Jump" + str(deviceID)) and $Area2D.get_overlapping_areas():
 		$Area2D.get_overlapping_areas()[0].grabber = self
 		$Area2D.get_overlapping_areas()[0].valid = false
-	if Input.is_action_just_released("Action" + str(deviceID)) and $Area2D.get_overlapping_areas():
+	if Input.is_action_just_released("Jump" + str(deviceID)) and $Area2D.get_overlapping_areas():
 		var grab_target = $Area2D.get_overlapping_areas()[0]
 		if not grab_target.valid:
 			grab_target.global_position = grab_target.original_position
