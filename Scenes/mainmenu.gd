088e43e466b9c588ef9bb79a89e86f12c016d773
Furiouspotato <realfuriouspotato@gmail.com>
2025-09-28 14:55:18 -0400

    added footsteps

--- Scenes/mainmenu.gd
+++ Scenes/mainmenu.gd
@@ -1,9 +1,10 @@
 extends Node2D
 
 func _input(event):
-	if event.is_action_pressed("Jump0"):
+	if event.is_action_pressed("Jump"):
+		print("amooooogus")
 		startgame()
 
 func startgame() -> void:
-	get_tree().change_scene_to_file("res://intro_cutscene.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Levels/intro_cutscene.tscn")
 	
